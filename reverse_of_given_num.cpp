614d12512cd76cc8209059d0756d8f4c5c2b8e92
shahwan786 <56104387+shahwan786@users.noreply.github.com>
2019-10-11 03:07:22 +0530

    reverse array printing

--- reverse_of_given_num.cpp
+++ reverse_of_given_num.cpp
@@ -1,9 +1,15 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
-{
-	int p,i,j,k=0;
-	scanf("%d",&i=k*10+j;
-	  i=i/10;
-	}
-	printf("%d",k);
+{ int n;
+ cin>>n;
+ int arr[n];
+ for(i=0;i<n;i++)
+ {
+	cin>>arr[n];
+ }
+ for(i=n-1;i>=0;i--)
+ {
+	 cout<<arr[i];
+ }
 }
