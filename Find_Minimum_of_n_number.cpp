9878fc59c0f399290d8f4c173ac98e27d9c3421e
hacktoberfest7 <57019433+hacktoberfest7@users.noreply.github.com>
2019-10-26 02:46:02 +0530

    for tshirt commit no 1

--- Find_Minimum_of_n_number.cpp
+++ Find_Minimum_of_n_number.cpp
@@ -1,20 +1,19 @@
 #include<stdio.h>
-using namespace std;
 int main(){
-	int n,i,min;
-	scanf("%d",&n);
+	int x,i,min;
+	scanf("%d",&x);
 	int a[n];
-	for(i=0;i<n;i++)
+	for(i=0;i<x;i++)
 	{
 	scanf("%d",&a[i]);
 	}
 	min=a[0];
-	for(i=0;i<n;i++)
+	for(i=0;i<x;i++)
 	{
 		if(min>a[i])
-		{rry
+		{min=a[i];
 		}
 	}
 	printf("%d",min);
-	
+	return 0;
 }
