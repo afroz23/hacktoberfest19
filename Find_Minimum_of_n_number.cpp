3b2a7e015d96bf49341e6f83aa74526de8cab3e8
Hrit99 <43970492+Hrit99@users.noreply.github.com>
2019-10-26 01:08:34 +0530

    deleted line

--- Find_Minimum_of_n_number.cpp
+++ Find_Minimum_of_n_number.cpp
@@ -3,15 +3,14 @@ using namespace std;
 int main(){
 	int n,i,min;
 	scanf("%d",&n);
-	int a[n];
+	int b[n];
 	for(i=0;i<n;i++)
 	{
-	scanf("%d",&a[i]);
+	scanf("%d",&b[i]);
 	}
 	min=a[0];
 	for(i=0;i<n;i++)
 	{
-		if(min>a[i])
 		{rry
 		}
 	}
