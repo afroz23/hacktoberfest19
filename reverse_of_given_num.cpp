f0dad6b68bc514896c06f69a4291db4d4cce342b
hacktoberfest24 <57046054+hacktoberfest24@users.noreply.github.com>
2019-10-27 00:14:02 +0530

    second pr

--- reverse_of_given_num.cpp
+++ reverse_of_given_num.cpp
@@ -1,9 +1,15 @@
-#include<stdio.h>
+
+#include<iostream>
+using namespace std;
 int main()
 {
 	int p,i,j,k=0;
-	scanf("%d",&i=k*10+j;
+	cin>>p;
+	for(i=0;i<p;i++)
+	{
+	i=k*10+j;
 	  i=i/10;
+		k++;
 	}
-	printf("%d",k);
+	cout<<k;
 }
