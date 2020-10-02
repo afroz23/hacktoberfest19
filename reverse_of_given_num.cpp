faeac9eaa87c1ecf8d16323cb21bf524e95bfdd8
saurav kumar <55906348+saurav1423@users.noreply.github.com>
2020-10-03 01:43:18 +0530

    variable changed

--- reverse_of_given_num.cpp
+++ reverse_of_given_num.cpp
@@ -3,13 +3,13 @@
 #iclude<math.h>
 void main()
 {
-    int num,r,sum=0,t;
+    int n,r,sum=0,t;
 
     printf("Input a number: ");
-    scanf("%d",&num);
+    scanf("%d",&n);
 
-    for(t=num;num!=0;num=num/10){
-         r=num % 10;
+    for(t=n;n!=0;n=n/10){
+         r=n % 10;
          sum=sum*10+r;
     }
 printf("The number in reverse order is : %d \n",sum);
