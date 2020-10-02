430f674eaafee5f20d2c13880073d6167a5fa8ec
saurav kumar <55906348+saurav1423@users.noreply.github.com>
2020-10-03 01:32:59 +0530

    updated a variable

--- Find_Minimum_of_n_number.cpp
+++ Find_Minimum_of_n_number.cpp
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,min;
-    scanf("%d",&n);
-    int a[n];
-    for(i=0;i<n;i++)
+    int t,i,min;
+    scanf("%d",&t);
+    int a[t];
+    for(i=0;i<t;i++)
     {
         scanf("%d",&a[i]);
     }
     min=a[0];
-    for(i=0;i<n;i++)
+    for(i=0;i<t;i++)
     {
         if(min>a[i])
 	{
