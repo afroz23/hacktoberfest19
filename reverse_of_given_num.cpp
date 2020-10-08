0f792cffa969795cfc0aef204f41e162089b5c49
firoz230 <72562708+firoz230@users.noreply.github.com>
2020-10-08 21:02:27 +0530

    Reversing Number using Reverse

--- reverse_of_given_num.cpp
+++ reverse_of_given_num.cpp
@@ -1,16 +1,23 @@
-#include <stdio.h>
-#include<conio.h>
-#iclude<math.h>
-void main()
-{
-    int num,r,sum=0,t;
+#include<iostream>
+using namespace std;
+int reverse_function(int x);
+int main(){
+   int num,rn;
+   printf("\nEnter any number:");
+   cin>>num;
+   rn=reverse_function(num);
+   cout<<"\nAfter reverse the no is :%d",rn;
+   return 0;
+}
 
-    printf("Input a number: ");
-    scanf("%d",&num);
-
-    for(t=num;num!=0;num=num/10){
-         r=num % 10;
-         sum=sum*10+r;
-    }
-printf("The number in reverse order is : %d \n",sum);
+int sum=0,rem;
+reverse_function(int x){
+   if(x){
+      rem=x%10;
+      sum=sum*10+rem;
+      reverse_function(x/10);
+   }
+   else
+      return sum;
+   return sum;
 }
